75cb7ec05e9bb60ae22200951bceddb91e5a84cc
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-27 18:29:55 +0300

    add card css

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './card.styles.css';
 
 export default function Card(props) {
   return (
-    <div>
+    <div className='card-container'>
       <h1>{props.data.name}</h1>
     </div>
   );
