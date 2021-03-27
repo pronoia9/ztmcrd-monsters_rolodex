b538ea61c25665ce0583c14a5a95db9d662403a4
Pronoia9 <19336274+pronoia9@users.noreply.github.com>
2021-03-27 19:03:26 +0300

    adjust css

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,8 +4,9 @@ import './card.styles.css';
 export default function Card(props) {
   return (
     <div className='card-container'>
-      <img alt='monster' src={`https://robohash.org/${props.data.id}?set=set2`} />
+      <img alt='monster' src={`https://robohash.org/${props.data.id}?set=set2&size=180x180`} />
       <h2>{props.data.name}</h2>
+      <p>{props.data.email}</p>
     </div>
   );
 }
